eb31b30af7c770825cf3ea47dfdcf46c1ed3c85e
Мария <oms1999@yandex.ru>
2024-01-21 15:04:38 +0300

    Modify class Vacancy

--- src/vacancy.py
+++ src/vacancy.py
@@ -8,21 +8,21 @@ class Vacancy:
         Создание экземпляра класса Vacancy
         """
 
-        self.name = name
-        self.url = url
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.schedule = schedule
-        self.town = town
+        self.__name = name
+        self.__url = url
+        self.__salary_from = salary_from
+        self.__salary_to = salary_to
+        self.__schedule = schedule
+        self.__town = town
 
     def __str__(self):
         """
         Возвращает информацию об экземпляре класса вакансии для пользователя
         """
-        return f'Вакансия на должность {self.name}\nЗарплата от {self.salary_from} до {self.salary_to} рублей'
+        return f'Вакансия на должность {self.__name}\nЗарплата от {self.__salary_from} до {self.__salary_to} рублей'
 
     def __lt__(self, other) -> bool:
         """
         Возвращает результат сравнения минимальной зарплаты двух экземпляров класса
         """
-        return self.salary_from < other.salary_from
+        return self.__salary_from < other.__salary_from
